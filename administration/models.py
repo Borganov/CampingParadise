f496d63f324eec90e6acc01830a19bd387e9ab32
Yann <yann.cla@gmail.com>
2020-04-24 12:25:35 +0200

    Models

--- administration/models.py
+++ administration/models.py
@@ -2,6 +2,7 @@ from django.contrib.gis.db import models
 
 # Create your models here.
 
+
 class Arbres (models.Model):
     gid = models.PositiveSmallIntegerField(primary_key=True)
     id = models.PositiveIntegerField(null=True)
@@ -9,3 +10,39 @@ class Arbres (models.Model):
 
     class Meta:
         db_table = "arbres"
+
+
+class Batiments (models.Model):
+    gid = models.PositiveSmallIntegerField(primary_key=True)
+    id = models.PositiveIntegerField(null=True)
+    geom = models.MultiPolygonField(srid=4326, null=True)
+
+    class Meta:
+        db_table = "batiments"
+
+
+class Emplacements (models.Model):
+    gid = models.PositiveSmallIntegerField(primary_key=True)
+    id = models.PositiveIntegerField(null=True)
+    geom = models.MultiPolygonField(srid=4326, null=True)
+
+    class Meta:
+        db_table = "emplacements"
+
+
+class Piscine (models.Model):
+    gid = models.PositiveSmallIntegerField(primary_key=True)
+    id = models.PositiveIntegerField(null=True)
+    geom = models.MultiPolygonField(srid=4326, null=True)
+
+    class Meta:
+        db_table = "piscine"
+
+
+class ZoneCamping (models.Model):
+    gid = models.PositiveSmallIntegerField(primary_key=True)
+    id = models.PositiveIntegerField(null=True)
+    geom = models.MultiPolygonField(srid=4326, null=True)
+
+    class Meta:
+        db_table = "zone-camping"
