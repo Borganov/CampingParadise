f040ed244895e36535280793c71ca8a8dbef2d01
Yann <yann.cla@gmail.com>
2020-04-24 12:33:03 +0200

    Update admin.py

--- administration/admin.py
+++ administration/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
-
+from django.contrib.gis import admin
+from .models import Arbres, Batiments, Emplacements, Piscine, ZoneCamping
 # Register your models here.
+
+admin.site.register(Arbres, admin.OSMGeoAdmin)
+admin.site.register(Batiments, admin.OSMGeoAdmin)
+admin.site.register(Emplacements, admin.OSMGeoAdmin)
+admin.site.register(Piscine, admin.OSMGeoAdmin)
+admin.site.register(ZoneCamping, admin.OSMGeoAdmin),
+
+
