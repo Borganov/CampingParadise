b7820ddd7a06336741de4652d7fcb2c91137a295
Bornatch <valentin.bornatici@gmail.com>
2020-05-01 18:43:06 +0200

    add truc

--- administration/util/emplacementDetails.py
+++ administration/util/emplacementDetails.py
@@ -1,10 +1,16 @@
 from administration.models import Piscines
+from administration.models import Emplacements
+from django.core.serializers import serialize
+import geopandas as gpd
+from administration.models import Batiments
 
 
 class emplacementDetails:
     has_tree = False
     closest_swimmingpool_dist = 2.4
+    closest_swimmingpool_id = 2
     closest_building_dist = 1.6
+    closest_building_id = 1
     area = 6.5
 
 
@@ -16,13 +22,40 @@ def getEmplacementDetails(idEmp):
 
 
 def hasTree(idEmp):
-    return True
+    return 'emplacement.id.toString()'
+
 
 def distancepiscine(idEmp):
     piscines = Piscines.objects.all()
 
+    return False
+
+
 def distanceBuilding(idEmp):
     pass
 
+
 def getArea(idEmp):
-    pass
+    """
+     emplacements = Emplacements.objects.filter(pk=idEmp).values()
+    test = emplacements[0]['geom'].json
+    truc = gpd.read_file(test)
+    print(test)
+    print(truc.crs)
+    print(truc.area)
+    print(truc.bounds)
+    truc = truc.to_crs({'init': 'epsg:3857'})
+    print(truc.crs)
+    print(truc.area)
+    print(truc.bounds)
+    :param idEmp:
+    :return:
+    """
+    emplacements = Emplacements.objects.filter(pk=idEmp)
+    emplacement = emplacements[0]
+    print(emplacement.geom.area)
+    print(emplacement.geom.boundary)
+
+
+
+
