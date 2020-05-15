e578ddb36137a2ecd9dc0ba6fa4c717998ed2cf3
Yann <yann.cla@gmail.com>
2020-05-15 09:42:15 +0200

    booked and unbooked

--- administration/util/zonesDetails.py
+++ administration/util/zonesDetails.py
@@ -1,15 +1,20 @@
 from administration.models import ZoneCamping, Emplacements
 
+
 class zonesDetails:
     zone = "-"
     area = -1
     emp_count = -1
     area_rate = -1
+    emp_booked = -1
+    emp_unbooked = -1
+
 
 def getZoneById(idZone):
     zones = ZoneCamping.objects.filter(gid=idZone)
     zone = zones[0]
     return zone
+
 
 def getZoneDetails(idZone):
     result = zonesDetails
@@ -17,7 +22,11 @@ def getZoneDetails(idZone):
     result.area = result.zone.geom.area
     result.emp_count = emplacementsCount(result.zone)
     result.area_rate = areaRate(result.zone)
+    booked_unbooked = countBookedUnbookedEmplacements(result.zone)
+    result.emp_booked = booked_unbooked['booked']
+    result.emp_unbooked = booked_unbooked['unbooked']
     return result
+
 
 def emplacementsCount(zone):
     emplacements = Emplacements.objects.all()
@@ -29,6 +38,7 @@ def emplacementsCount(zone):
             count += 1
     return count
 
+
 def areaRate(zone):
     zones = ZoneCamping.objects.all()
     area_tot = 0
@@ -37,5 +47,20 @@ def areaRate(zone):
 
     area_zone = zone.geom.area
 
-    result = area_zone*100/area_tot
+    result = area_zone * 100 / area_tot
     return result
+
+
+def countBookedUnbookedEmplacements(zone):
+    emplacements = Emplacements.objects.all()
+    countBooked = 0
+    countUnbooked = 0
+    polygon = zone.geom
+    for emp in emplacements:
+        point = emp.geom.centroid
+        if polygon.contains(point) and emp.booked == 1:
+            countBooked += 1
+        elif polygon.contains(point) and emp.booked != 1:
+            countUnbooked += 1
+
+    return {'booked': countBooked, 'unbooked': countUnbooked}
