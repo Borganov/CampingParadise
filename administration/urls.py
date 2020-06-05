78475238f3c24f91c2f575088fcadf46367c39e9
Bornatch <valentin.bornatici@gmail.com>
2020-06-05 11:03:16 +0200

    clean

--- administration/urls.py
+++ administration/urls.py
@@ -2,19 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('arbre/<int:gid>', views.arbre, name='arbre'),
+
     path('arbres.json', views.arbresjson, name='arbresjson'),
-    path('arbres/', views.arbres, name='arbres'),
-    path('emplacement/<int:gid>', views.emplacement, name='emplacement'),
     path('emplacements.json', views.emplacementsAlljson, name='emplacementsjson'),
     path('zones.json', views.zonesjson, name='zonesjson'),
     path('emplacements/<int:id>', views.emplacements, name='emplacements'),
     path('zones/<int:id>', views.zones, name='zones'),
     path('batiments.json', views.batimentsjson, name='batimentsjson'),
-    path('batiments/', views.batiments, name='batiments'),
     path('piscines.json', views.piscinesjson, name='piscinesjson'),
-    path('piscines/', views.piscines, name='piscines'),
     path('zoneCamping.json', views.zonecampingjson, name='zonecampingjson'),
-    path('zoneCamping/', views.zonecamping, name='zonecamping'),
     path('accueil/', views.accueil, name='accueil')
 ]
--- administration/views.py
+++ administration/views.py
@@ -11,26 +11,15 @@ from random import randint
 
 # Create your views here.
 
-def arbre(request, gid):
-    arbres = Arbres.objects.filter(pk=gid)
-    emplacements = Emplacements.objects.filter(pk=gid)
-    return render(request, 'camping/arbre.html', {'arbreobj': arbres[0], 'emplacementobj': emplacements[0]})
-
-
 def arbresjson(request):
     arbres = Arbres.objects.all()
     ser = serialize('geojson', arbres, geometry_field='geom', fields=('gid', 'id'))
     return HttpResponse(ser)
 
 
-def arbres(request):
-    context = {}
-    return render(request, 'camping/arbres.html', context)
-
-
-def emplacement(request, gid):
-    emplacements = Emplacements.objects.filter(pk=gid)
-    return render(request, 'camping/emplacement.html', {'emplacementobj': emplacements[0]})
+def emplacements(request, id):
+    eff = ed.getEmplacementDetails(id)
+    return render(request, 'camping/emplacements.html', {'emplacementobj': eff})
 
 
 def emplacementsAlljson(request):
@@ -45,16 +34,6 @@ def zonesjson(request):
     return HttpResponse(ser)
 
 
-def emplacementdetails(request):
-    emplacementdetails = ed.getEmplacementDetails(1)
-    return render(request, 'camping/emplacement.html', {'emplacementobj': emplacementdetails[0]})
-
-
-def emplacements(request, id):
-    eff = ed.getEmplacementDetails(id)
-    return render(request, 'camping/emplacements.html', {'emplacementobj': eff})
-
-
 def zones(request, id):
     zone = zd.getZoneDetails(id)
     return render(request, 'camping/zones.html', {'zone': zone})
@@ -66,30 +45,16 @@ def batimentsjson(request):
     return HttpResponse(ser)
 
 
-def batiments(request):
-    context = {}
-    return render(request, 'camping/batiments.html', context)
-
 def piscinesjson(request):
     piscines = Piscines.objects.all()
     ser = serialize('geojson', piscines, geometry_field='geom', fields=('gid', 'id'))
     return HttpResponse(ser)
 
 
-def piscines(request):
-    context = {}
-    return render(request, 'camping/piscines.html', context)
-
-
 def zonecampingjson(request):
     zonecamping = ZoneCamping.objects.all()
     ser = serialize('geojson', zonecamping, geometry_field='geom', fields=('gid', 'id'))
     return HttpResponse(ser)
-
-
-def zonecamping(request):
-    context = {}
-    return render(request, 'camping/zonecamping.html', context)
 
 
 def accueil(request):
